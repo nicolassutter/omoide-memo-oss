92e7ceed80d742eeab1a600592397f9f756453c6
nicolassutter <contact.sutter.nicolas@gmail.com>
2026-09-11 15:08:11 +0200

    feat: add basic telemetry-api go mod

---
+++ packages/telemetry-api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/go-fuego/fuego"
+)
+
+func main() {
+	s := fuego.NewServer()
+
+	fuego.Get(s, "/", helloWorld)
+
+	s.Run()
+}
+
+func helloWorld(c fuego.ContextNoBody) (string, error) {
+	return "Hello, World!", nil
+}
